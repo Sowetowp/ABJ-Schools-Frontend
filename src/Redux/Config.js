ed239a7ed000b204f795501b566d31f7ffa2809c
Abdulmalik <ayodejiamzat@gmail.com>
2024-02-14 01:21:23 +0100

    ff

--- src/Redux/Config.js
+++ src/Redux/Config.js
@@ -1,6 +1,6 @@
 const config = {
-    // liveUrl: "https://crazy-slug-hoodie.cyclic.cloud/api"
-    liveUrl: "http://localhost:5000/api"
+    liveUrl: "https://cute-leotard-toad.cyclic.app/api"
+    // liveUrl: "http://localhost:5000/api"
 }
 
 export {config}
